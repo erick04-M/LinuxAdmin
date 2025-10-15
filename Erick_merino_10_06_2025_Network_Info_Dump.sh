367546dbd10b626fc4f7fc72ec7c5e81ae82f0a3
erick04-M <erickmerino1012@gmail.com>
2025-10-15 17:16:01 -0400

    Add files via upload

---
+++ Erick_merino_10_06_2025_Network_Info_Dump.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/sh
+# Author: Erick Merino
+# Date: 10-06-2025
+# Network Information Dump
+# To use this, you need to make the file executable with chmod 777, and once done, you can use ./*scriptname and it will execute it
+# sudo apt install traceroute nmap to use commands
+
+
+{ echo " This is a networks Information dump about our server" 
+#for all of the lol cats following an echo we are doing it for readability so that we can see it more clearly
+#we are going to ping google just to see if we are connected to the internet
+echo " we are connected to the internet and are able to communicate with google"
+ping -c 4 google.com
+
+#we are going to do ip -s link show our status for our interfaces as well as showing their stats like packets, errors, and etc.
+echo " we are showing our interface statistics as well as their status"
+ip -s link show
+
+echo " This is an active summary for our netowork socket related information"
+#this command will show us all of our tcp connections together with their timers, however it is only the active ones that are shown, if you want to list all
+#all of the listening TCP connections you can run ss -lt, or for all active tcp do ss -to and UDP is -ua 
+ss -s
+
+echo " This is our route to our default gateway"
+traceroute 10.0.0.1
+
+echo "this is our host name for the server followed by the ip address"
+hostname
+hostname -i
+
+echo "this is nmap and it will show us our entire network and present to us information about it"
+#this command will show us network status, and the connections, as well as information about the ports. It is a very powerful command as it can be used for 
+#security scans, network audits and so much more. 				
+nmap 10.0.0.50
+
+
+echo "saving results from script into Log File"
+} | tee ./Logs/Erick_Merino_Data_Dump_log$(date +"%m-%d-%Y").txt
+#All of the results of this script will be saved into a log file called Erick_Merino_data_dump_log and this file will be in the Logs folder that is in the
+#directory of Scripts
+
